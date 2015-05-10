b0453c62b78f9ff5211f9357ee9738dee9af7719
studiowangfei <studiowangfei@gmail.com>
2015-05-10 00:01:12 -0400

    first push

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,14 +1,1 @@
-$(document).ready(function(){
-  $(window).on('beforeunload', function() {
-    $(window).scrollTop(0);
-  });
-
-  // no feature, adding margin
-  $('.bottom-section').css('margin-top', '224px');
-
-  $('#qrCode').click(function(){
-    $('#qrCode_img').fadeToggle('fast');
-    $('#sfphoto').fadeToggle('fast');
-  });
-
-});
+$(document).ready(function(){$(window).on("beforeunload",function(){$(window).scrollTop(0)}),$(".bottom-section").css("margin-top","224px"),$("#qrCode").click(function(){$("#qrCode_img").fadeToggle("fast"),$("#sfphoto").fadeToggle("fast")})});
