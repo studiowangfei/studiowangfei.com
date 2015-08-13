5f60a70ed84252bc7489cb122a2de24727a63c04
studiowangfei <studiowangfei@gmail.com>
2015-08-13 03:04:29 -0700

    update

--- public/js/core_framework.js
+++ public/js/core_framework.js
@@ -1,1 +1,146 @@
-var pageType;pageType=document.getElementsByClassName("feature_container")[0]?"projectEach":$(".page_type").text();var urlMap,projectPage,aboutPage,blogPage,contactPage;"projectIndex"==pageType||"projectEach"==pageType?(projectPage="current",aboutPage=null,blogPage=null,contactPage=null):"about"==pageType?(projectPage=null,aboutPage="current",blogPage=null,contactPage=null):"blog"==pageType?(projectPage=null,aboutPage=null,blogPage="current",contactPage=null):(projectPage=null,aboutPage=null,blogPage=null,contactPage="current"),urlMap="projectEach"==pageType?"../":"";var TopContainer=React.createClass({displayName:"TopContainer",render:function(){return React.createElement("div",null,React.createElement("div",{className:"top-placeholder"}),React.createElement("div",{className:"top-container"},React.createElement("div",{className:"top-section"},React.createElement("div",{className:"left"},React.createElement("a",{id:"logo",href:"http://www.studiowangfei.com/"},"Studiowangfei")),React.createElement("ul",{className:"right"},React.createElement("li",null,React.createElement("a",{className:"item",id:projectPage,href:urlMap+"project.html"}," Project")),React.createElement("li",null,React.createElement("a",{className:"item",id:aboutPage,href:urlMap+"about.html"}," About")),React.createElement("li",null,React.createElement("a",{className:"item",id:blogPage,href:urlMap+"blog.html"}," Blog")),React.createElement("li",null,React.createElement("a",{className:"item",id:contactPage,href:urlMap+"contact.html"}," Contact"))),React.createElement("div",{id:"menu-icon"})),React.createElement("ul",{className:"hiddenList"},React.createElement("li",null,React.createElement("a",{className:"item",href:urlMap+"project.html"}," Project")),React.createElement("li",null,React.createElement("a",{className:"item",href:urlMap+"about.html"}," About")),React.createElement("li",null,React.createElement("a",{className:"item",href:urlMap+"blog.html"}," Blog")),React.createElement("li",null,React.createElement("a",{className:"item",href:urlMap+"contact.html"}," Contact")))))}});React.render(React.createElement(TopContainer,null),document.getElementById("topContainer")),$(document).ready(function(){function e(){var e=$(window).scrollTop();0==e&&"none"==$("#menu-icon").css("display")?$(".top-container").removeClass("addColorShadow"):$(".top-container").addClass("addColorShadow")}function t(){a>840?($("#logo").css("font-size","14px"),$("#menu-icon").css("display","none"),$(".hiddenList").css("display","none"),$(".top-container").removeClass("extendHeight"),$(".right").css("display","flex"),$(window).scroll(function(){e()})):($("#logo").css("font-size","18px"),$(".top-container").addClass("addColorShadow"),$("#menu-icon").css("display","initial"),$(".right").css("display","none"))}var a=$(".main-container").width();$(window).on("beforeunload",function(){$(window).scrollTop(0)}),$("#menu-icon").click(function(){$("#menu-icon").toggleClass("menu-icon-transform"),$(".top-container").toggleClass("extendHeight"),$(".hiddenList").fadeToggle(750)}),e(),t(),$(window).resize(function(){a=$(".main-container").width(),$("#menu-icon").removeClass("menu-icon-transform"),t(),e()})});
+// do NOT write comment like"// xxxxxxxx" in JSX syntax, it cannot be parsed or skipped
+
+// get page type info by testing if 'feature_container' exists,
+// and decide the  current section from DOM
+var pageType;
+if (!document.getElementsByClassName('feature_container')[0]){
+  pageType = $('.page_type').text();
+}else{
+  pageType = "projectEach";
+}
+
+// deciding url path and the current section page by pageType
+var urlMap;
+var projectPage, aboutPage, blogPage, contactPage;
+
+if (pageType == "projectIndex" || pageType == "projectEach"){
+  projectPage = "current";
+  aboutPage = null;
+  blogPage = null;
+  contactPage = null;
+}else if (pageType == "about"){
+  projectPage = null;
+  aboutPage = "current";
+  blogPage = null;
+  contactPage = null;
+}else if (pageType == "blog"){
+  projectPage = null;
+  aboutPage = null;
+  blogPage = "current";
+  contactPage = null;
+}else{
+  projectPage = null;
+  aboutPage = null;
+  blogPage = null;
+  contactPage = "current";
+}
+
+if (pageType == "projectEach"){
+  urlMap = "../";
+}else{
+  urlMap = "";
+}
+
+// top bar framework for index and project pages
+// to make React-constructed element perfectly match html-constructed element spacing, add " " before each section name
+// TopContainer responsive to window width, see setting in css
+var TopContainer = React.createClass({displayName: "TopContainer",
+  render: function(){
+    return (
+      React.createElement("div", null, 
+        React.createElement("div", {className: "top-placeholder"}
+        ), 
+        React.createElement("div", {className: "top-container"}, 
+          React.createElement("div", {className: "top-section"}, 
+            React.createElement("div", {className: "left"}, 
+              React.createElement("a", {id: "logo", href: "http://www.studiowangfei.com/"}, "Studiowangfei")
+            ), 
+            React.createElement("ul", {className: "right"}, 
+              React.createElement("li", null, React.createElement("a", {className: "item", id: projectPage, href: urlMap+"project.html"}, " Project")), 
+              React.createElement("li", null, React.createElement("a", {className: "item", id: aboutPage, href: urlMap+"about.html"}, " About")), 
+              React.createElement("li", null, React.createElement("a", {className: "item", id: blogPage, href: urlMap+"blog.html"}, " Blog")), 
+              React.createElement("li", null, React.createElement("a", {className: "item", id: contactPage, href: urlMap+"contact.html"}, " Contact"))
+            ), 
+            React.createElement("div", {id: "menu-icon"})
+          ), 
+
+          React.createElement("ul", {className: "hiddenList"}, 
+            React.createElement("li", null, React.createElement("a", {className: "item", href: urlMap+"project.html"}, " Project")), 
+            React.createElement("li", null, React.createElement("a", {className: "item", href: urlMap+"about.html"}, " About")), 
+            React.createElement("li", null, React.createElement("a", {className: "item", href: urlMap+"blog.html"}, " Blog")), 
+            React.createElement("li", null, React.createElement("a", {className: "item", href: urlMap+"contact.html"}, " Contact"))
+          )
+
+        )
+      )
+    );
+  }
+});
+
+React.render(
+  React.createElement(TopContainer, null),
+  document.getElementById('topContainer')
+);
+
+// standard controller for all pages
+$(document).ready(function(){
+
+  // back to top when reload
+  $(window).on('beforeunload', function(){
+    $(window).scrollTop(0);
+  });
+
+  function hideCascade(){
+    var containerWidth = $(window).width();
+    if (containerWidth > 720){
+      $('.top-container')
+      .css('height', '64px');
+
+      $('#menu-icon')
+      .removeClass('menu-icon-transform');
+
+      $('.hiddenList')
+      .css('display', 'none');
+    }
+  }
+
+  // top container color and shadow effect
+  function headerEffect(){
+    var topMargin = $(window).scrollTop();
+    if (topMargin == 0 && $('#menu-icon').css('display') == 'none'){
+      $('.top-container')
+      .removeClass('addColorShadow');
+    }else{
+      $('.top-container')
+      .addClass('addColorShadow');
+    }
+  }
+
+  hideCascade();
+  headerEffect();
+  $(window).scroll(function(){
+    headerEffect();
+    hideCascade();
+  });
+
+  $(window).resize(function(){
+    headerEffect();
+    hideCascade();
+  });
+
+  // click the menu icon to expand the list
+  $('#menu-icon').click(function(){
+    $('#menu-icon')
+    .toggleClass('menu-icon-transform')
+
+    if($('.top-container').height() < 244){
+      $('.top-container').css('height', '248px');
+    }else{
+      $('.top-container').css('height', '64px');
+    }
+
+    $('.hiddenList').fadeToggle(750);
+  });
+
+
+});
