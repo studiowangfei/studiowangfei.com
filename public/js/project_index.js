44f841d549843599c0cb56511a65a48f35faa746
studiowangfei <studiowangfei@gmail.com>
2015-09-21 03:51:18 -0700

    pushed new project

--- public/js/project_index.js
+++ public/js/project_index.js
@@ -1,1 +1,97 @@
-$(document).ready(function(){function c(){var a=$(window).width();a>=1032?$(".icon_container").css("width","960px"):a>=792?$(".icon_container").css("width","720px"):a>=552?$(".icon_container").css("width","480px"):$(".icon_container").css("width","240px")}$(window).on("beforeunload",function(){$(window).scrollTop(0)});for(var a=0;18>a;a++){var b;0===a?b="project/mylio.html":1===a?b="project/sombim.html":2===a?b="project/linkedin.html":3===a?b="project/zoom.html":4===a?b="project/nycsub.html":5===a?b="project/taobao_2.html":6===a?b="project/sfpopos.html":7===a?b="project/webframe.html":8===a?b="project/taobao.html":9===a?b="project/inin.html":10===a?b="project/work.html":11===a?b="project/anotherleap.html":12===a?b="project/urbanrural.html":13===a?b="project/impressionism.html":14===a?b="project/holistic.html":15===a?b="project/drawit.html":16===a&&(b="project/closer.html"),$(".icon_container").append('<div class="flipper" id="flipper_'+a+'"><a href='+b+'><div class="front"></div></a><div class="back"></div></div>'),$("#flipper_"+a+" .front").css("background-image",'url("img/project/project_icon/project_sprite_front.png")').css("background-repeat","no-repeat").css("background-position",-220*a+"px 0px"),$("#flipper_"+a+" .back").css("background-image",'url("img/project/project_icon/project_sprite_back.png")').css("background-repeat","no-repeat").css("background-position",-220*a+"px 0px")}$("#flipper_17").empty(),$(".front").mouseenter(function(){$(".back").css("opacity","1")}),c(),$(window).resize(function(){c()})});
+$(document).ready(function(){
+  $(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
+  });
+
+   //flipper append, using sprite method
+  for(var i=0;i<20;i++){
+    var projectUrl;
+    if(i===0){
+      projectUrl="project/mylio.html"
+    }else if(i===1){
+      projectUrl="project/sombim.html"
+    }else if(i===2){
+      projectUrl="project/blooday.html"
+    }else if(i===3){
+      projectUrl="project/linkedin.html"
+    }else if(i===4){
+      projectUrl="project/zoom.html"
+    }else if(i===5){
+      projectUrl="project/nycsub.html"
+    }else if(i===6){
+      projectUrl="project/taobao_2.html"
+    }else if(i===7){
+      projectUrl="project/sfpopos.html"
+    }else if(i===8){
+      projectUrl="project/webframe.html"
+    }else if(i===9){
+      projectUrl="project/taobao.html"
+    }else if(i===10){
+      projectUrl="project/inin.html"
+    }else if(i===11){
+      projectUrl="project/work.html"
+    }else if(i===12){
+      projectUrl="project/anotherleap.html"
+    }else if(i===13){
+      projectUrl="project/urbanrural.html"
+    }else if(i===14){
+      projectUrl="project/impressionism.html"
+    }else if(i===15){
+      projectUrl="project/holistic.html"
+    }else if(i===16){
+      projectUrl="project/drawit.html"
+    }else if(i===17){
+      projectUrl="project/closer.html"
+    }
+
+    $('.icon_container').append('<div class="flipper" id="flipper_'+i+'">'
+      +'<a href='+projectUrl+'><div class="front"></div></a><div class="back"></div></div>');
+
+    $('#flipper_'+i+' .front')
+    .css('background-image', 'url("img/project/project_icon/project_sprite_front.png")')
+    .css('background-repeat','no-repeat')
+    .css('background-position', -220*i+'px 0px');
+
+    $('#flipper_'+i+' .back')
+    .css('background-image', 'url("img/project/project_icon/project_sprite_back.png")')
+    .css('background-repeat','no-repeat')
+    .css('background-position', -220*i+'px 0px');
+  };
+
+  // adjust last icon for last row (not necessary when n=4x)
+  // hide but still maintain that placeholder for flex justify-content: space-between
+  $('#flipper_18, #flipper_19').empty();
+
+  // change opacity to show title when hover
+  $('.front').mouseenter(
+    function(){
+      $('.back')
+      .css('opacity', '1')
+    }
+  );
+
+  // project icons layout setting
+  // responsive to window width
+  function projectIconLayout(){
+    var containerWidth = $(window).width();
+    if (containerWidth >= 1032){
+      $('.icon_container')
+      .css('width', '960px');
+    }else if (containerWidth >= 792){
+      $('.icon_container')
+      .css('width', '720px');
+    }else if (containerWidth >= 552){
+      $('.icon_container')
+      .css('width', '480px');
+    }else{
+      $('.icon_container')
+      .css('width', '240px');
+    }
+  }
+
+  projectIconLayout();
+  $(window).resize(function(){
+    projectIconLayout();
+  });
+
+});
