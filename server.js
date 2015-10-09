b8d2900b2681bd20e55a816f8d432662b02fbb12
studiowangfei <studiowangfei@gmail.com>
2015-10-08 22:55:19 -0700

    fixed data linking bugs

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 var express = require('express');
-var server = express();
+var app = express();
 
-server.set('port', (process.env.PORT || 5000));
-server.use(express.static(__dirname + '/public', { maxAge: 0 }));
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-server.get('/', function(request, response){
-  response.sendFile('public/project.html', { root: __dirname });
-});
-
-server.listen(server.get('port'), function(){
-  console.log('The server is running at ' + server.get('port'));
+app.listen(app.get('port'), function() {
+  console.log('The server is running at ' + app.get('port'));
 });
